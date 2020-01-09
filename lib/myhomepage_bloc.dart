94ec083148dfc61e916e494f319aa384ac5e24f9
Athirson <athirsonleandro06@gmail.com>
2020-01-09 02:14:26 -0300

    Gerência de Estado

---
+++ lib/myhomepage_bloc.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'package:dio/dio.dart';
+import 'package:gerencia_de_estado/endereco_model.dart';
+
+class MyHomePageBloc{
+  // ignore: close_sinks
+  final StreamController<String> _streamController = StreamController<String>.broadcast();
+  Sink<String> get input => _streamController.sink;
+  Stream<EnderecoModel> get output =>
+      _streamController.stream
+          .where((cep) => cep.length > 7)
+          .asyncMap((cep) => _seachCep(cep));
+
+  String url(String cep) => "https://viacep.com.br/ws/$cep/json/";
+
+
+  Future<EnderecoModel> _seachCep(String cep) async{
+    Response response = await Dio().get(url(cep));
+   return EnderecoModel.fromJson(response.data);
+
+  }
+
+  }
+
+
